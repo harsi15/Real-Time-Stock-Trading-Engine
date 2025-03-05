2ca4e1ba747af0874f6ddf90698eace7a6851a1f
Simmy <dhillonharsimran192@gmail.com>
2025-03-05 18:15:39 -0500

    Updating Order.java class

--- Real TIme Stock Trading/src/Order.java
+++ Real TIme Stock Trading/src/Order.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Order {
+//Creating a base Structure of our order class
+public class Order {
+
+    int stockQuantity;
+    int stockPrice;
+    char[] stockTickerSymbol;
+    char[] stockOrderType;
+
+    public Order(char[] stockOrderType, char[] stockTickerSymbol, int stockQuantity, int stockPrice){
+        this.stockOrderType = stockOrderType;
+        this.stockTickerSymbol = stockTickerSymbol;
+        this.stockQuantity = stockQuantity;
+        this.stockPrice = stockPrice;
+    }
 }
