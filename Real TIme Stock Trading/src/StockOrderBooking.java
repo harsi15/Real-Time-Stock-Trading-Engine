478a5907a1e4531c36f1f58f5f33f5d410673976
Simmy <dhillonharsimran192@gmail.com>
2025-03-05 18:42:00 -0500

    Updating StockOrderBooking.java to compare the stock Tickers

--- Real TIme Stock Trading/src/StockOrderBooking.java
+++ Real TIme Stock Trading/src/StockOrderBooking.java
@@ -1,5 +1,18 @@
 public class StockOrderBooking {
     private Order head = null;
+
+    //To compare the stocks we have written a code reuse the code logic
+    private boolean compareStockTickers(char[] stockTicker1, char[] stockTicker2){
+        if(stockTicker1.length !=stockTicker2.length){
+            return false;
+        }
+        for(int i = 0; i< stockTicker1.length; i++){
+            if(stockTicker1[i] != stockTicker2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
     public synchronized void addOrder(char[] stockOrderType, char[] stockTickerSymbol, int stockQuantity, int stockPrice){
         Order stockOrder = new Order(stockOrderType, stockTickerSymbol, stockQuantity, stockPrice);
         stockOrder.next = head;
